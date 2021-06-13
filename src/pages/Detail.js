98d20f55e1ef8ba1913722dfb3d519219eba81be
Filip Katusin <filip.katusin@gmail.com>
2021-06-13 15:48:36 +0200

    dokoncenie detail karty

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -4,6 +4,7 @@ import useFetchCountries from "../useFetchCountries";
 import "../css/detail.css";
 import { Link } from "react-router-dom";
 import { MdKeyboardBackspace } from "react-icons/md";
+import lookup from "country-code-lookup";
 
 const Detail = () => {
   const { country } = useParams();
@@ -23,8 +24,6 @@ const Detail = () => {
   //   } = countries[0];
   //chcel som takto ale neviem preco to takto neislo :(
 
-  //   console.log(countries[0]);
-
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
@@ -32,6 +31,8 @@ const Detail = () => {
   if (isError) {
     return <h2>Error</h2>;
   }
+
+  console.log(countries[0].borders);
 
   return (
     <div className="detail">
@@ -42,55 +43,82 @@ const Detail = () => {
         </button>
       </Link>
 
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${countries[0].flag})`,
-        }}
-      ></div>
-      <h1>{countries[0].name}</h1>
-      <div className="text-section">
-        <p>
-          <span>Native name:</span>
-          {countries[0].nativeName}
-        </p>
-        <p>
-          <span>Population:</span>
-          {countries[0].population}
-        </p>
-        <p>
-          <span>Region:</span>
-          {countries[0].region}
-        </p>
-        <p>
-          <span>Sub region:</span>
-          {countries[0].subregion}
-        </p>
-        <p>
-          <span>Capital:</span>
-          {countries[0].capital}
-        </p>
+      <div className="wrapper">
+        <div
+          className="image"
+          style={{
+            backgroundImage: `url(${countries[0].flag})`,
+          }}
+        ></div>
+
+        <div className="text-section-container">
+          <h1>{countries[0].name}</h1>
+          <div className="text-section">
+            <p>
+              <span>Native name:</span>
+              {countries[0].nativeName}
+            </p>
+            <p>
+              <span>Population:</span>
+              {countries[0].population}
+            </p>
+            <p>
+              <span>Region:</span>
+              {countries[0].region}
+            </p>
+            <p>
+              <span>Sub region:</span>
+              {countries[0].subregion}
+            </p>
+            <p>
+              <span>Capital:</span>
+              {countries[0].capital}
+            </p>
+          </div>
+
+          <div className="text-section">
+            <p>
+              <span>Top level domain:</span>
+              {countries[0].topLevelDomain}
+            </p>
+            <p>
+              <span>Currencies:</span>
+              {countries[0].currencies.map((currency) => {
+                return (
+                  <span className="currency" key={currency.code}>
+                    {currency.name}
+                  </span>
+                );
+              })}
+            </p>
+            <p>
+              <span>Languages:</span>
+              {countries[0].languages.map((language, index) => {
+                return (
+                  <span key={index} className="language">
+                    {countries[0].languages.length - 1 === index
+                      ? language.name
+                      : `${language.name},`}
+                  </span>
+                );
+              })}
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="text-section">
-        <p>
-          <span>Top level domain:</span>
-          {countries[0].topLevelDomain}
-        </p>
-        <p>
-          <span>Currencies:</span>
-          {/* {countries[0].currencies} */}
-        </p>
-        <p>
-          <span>Languages:</span>
-          {countries[0].topLevelDomain}
-        </p>
-      </div>
-
-      <h2>Border Countries:</h2>
-      <div className="border-container">
+      {countries[0].borders.length !== 0 && <h2>Border Countries:</h2>}
+      <div className="borders-container">
         {countries[0].borders.map((border, index) => {
-          return <p key={index}>{border}</p>;
+          return (
+            <Link
+              to={`/detail/${lookup.byIso(border).country}`}
+              className="border"
+              key={index}
+            >
+              {lookup.byIso(border).country}
+            </Link>
+          );
         })}
       </div>
     </div>
